a1f8a6c5b7a024bc2b0ba9224fb47c870d6a883a
JML42 <joshua.mcculloch@sky.uk>
2023-12-14 12:22:46 +0000

    Fixes switch to break out once src and allText set

--- src/main/java/com/homeapp/nonsense_BE/services/ImageService.java
+++ src/main/java/com/homeapp/nonsense_BE/services/ImageService.java
@@ -49,23 +49,23 @@ public class ImageService {
     private Image chooseFrameImage(FullBike b) {
         Image i = new Image(0, "Frame", "no_image.png", "No Frame Selected");
         switch (b.getFrame().getFrameStyle()) {
-            case TOUR: {
+            case TOUR -> {
                 i.setSrc("tour_xxx.png");
                 i.setAltText("Tour Frame xxx");
             }
-            case SINGLE_SPEED: {
+            case SINGLE_SPEED -> {
                 i.setSrc("fixie_frame.png");
                 i.setAltText("Single Speed Frame");
             }
-            case ROAD: {
+            case ROAD -> {
                 i.setSrc("road_xxx.png");
                 i.setAltText("Road Frame xxx");
             }
-            case GRAVEL: {
+            case GRAVEL -> {
                 i.setSrc("gravel_disc.png");
                 i.setAltText("Gravel Frame xxx");
             }
-            default: {
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Frame Selected");
             }
@@ -83,25 +83,26 @@ public class ImageService {
     private Image chooseBarImage(FullBike b) {
         Image i = new Image(1, "Bars", "no_image.png", "No Bars Selected");
         switch (b.getHandleBarType()) {
-            case FLARE: {
+            case FLARE -> {
                 i.setSrc("flared_bars.png");
                 i.setAltText("Flared Bars");
             }
-            case DROPS: {
+            case DROPS -> {
                 i.setSrc("drop_bars.png");
                 i.setAltText("Drop Bars");
             }
-            case BULLHORNS: {
+            case BULLHORNS -> {
                 i.setSrc("bullhorns_bars.png");
                 i.setAltText("Bull Horn Bars");
             }
-            case FLAT: {
+            case FLAT -> {
                 i.setSrc("flat_bars.png");
                 i.setAltText("Flat Bars");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Bars Selected");
+            }
         }
         return i;
     }
@@ -110,7 +111,7 @@ public class ImageService {
         Image i = new Image(2, "Brakes", "shimano_xxx.png", "Shimano xxx Brakes");
         if (b.getBrakeType().getName().equals("NOT_REQUIRED")) {
             i.setSrc("no_image.png");
-            i.setAltText("No Brakes Required! Brave :) ");
+            i.setAltText("No Brakes Required! Brave ->) ");
         } else if (b.getBrakeType().getName().equals("Rim")) {
             i.setSrc(i.getSrc().replace("xxx", "rim"));
             i.setAltText(i.getAltText().replace("xxx", "Rim"));
@@ -124,29 +125,30 @@ public class ImageService {
     private Image chooseSTIShiftersImage(FullBike b) {
         Image i = new Image(3, "Shifters", "no_image.png", "No Gears Selected");
         switch ((int) b.getNumberOfRearGears()) {
-            case 1: {
+            case 1 -> {
                 i.setSrc("1_STI.png");
                 i.setAltText("Single Speed STI");
             }
-            case 9: {
+            case 9 -> {
                 i.setSrc("9_STI.png");
                 i.setAltText("Nine Speed STI");
             }
-            case 10: {
+            case 10 -> {
                 i.setSrc("10_STI.png");
                 i.setAltText("Ten Speed STI");
             }
-            case 11: {
+            case 11 -> {
                 i.setSrc("11_STI.png");
                 i.setAltText("Eleven Speed STI");
             }
-            case 12: {
+            case 12 -> {
                 i.setSrc("12_STI.png");
                 i.setAltText("Twelve Speed STI");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
@@ -154,25 +156,26 @@ public class ImageService {
     private Image chooseTriggerShiftersImage(FullBike b) {
         Image i = new Image(3, "Shifters", "no_image.png", "No Gears Selected");
         switch ((int) b.getNumberOfRearGears()) {
-            case 9: {
+            case 9 -> {
                 i.setSrc("9_trigger.png");
                 i.setAltText("Nine Speed STI");
             }
-            case 10: {
+            case 10 -> {
                 i.setSrc("10_trigger.png");
                 i.setAltText("Ten Speed STI");
             }
-            case 11: {
+            case 11 -> {
                 i.setSrc("11_trigger.png");
                 i.setAltText("Eleven Speed STI");
             }
-            case 12: {
+            case 12 -> {
                 i.setSrc("12_trigger.png");
                 i.setAltText("Twelve Speed STI");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
@@ -180,25 +183,26 @@ public class ImageService {
     private Image chooseRearDerailleurImage(FullBike b) {
         Image i = new Image(5, "Rear-Dearailleur", "no_image.png", "No Gears Selected");
         switch ((int) b.getNumberOfRearGears()) {
-            case 9: {
+            case 9 -> {
                 i.setSrc("9_derailleur.png");
                 i.setAltText("Nine Speed Derailleur");
             }
-            case 10: {
+            case 10 -> {
                 i.setSrc("10_derailleur.png");
                 i.setAltText("Ten Speed Derailleur");
             }
-            case 11: {
+            case 11 -> {
                 i.setSrc("11_derailleur.png");
                 i.setAltText("Eleven Speed Derailleur");
             }
-            case 12: {
+            case 12 -> {
                 i.setSrc("12_derailleur.png");
                 i.setAltText("Twelve Speed Derailleur");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
@@ -206,29 +210,30 @@ public class ImageService {
     private Image chooseChainImage(FullBike b) {
         Image i = new Image(6, "Chain", "no_image.png", "No Gears Selected");
         switch ((int) b.getNumberOfRearGears()) {
-            case 1: {
+            case 1 -> {
                 i.setSrc("1_chain.png");
                 i.setAltText("Single Speed Chain");
             }
-            case 9: {
+            case 9 -> {
                 i.setSrc("9_chain.png");
                 i.setAltText("Nine Speed Chain");
             }
-            case 10: {
+            case 10 -> {
                 i.setSrc("10_chain.png");
                 i.setAltText("Ten Speed Chain");
             }
-            case 11: {
+            case 11 -> {
                 i.setSrc("11_chain.png");
                 i.setAltText("Eleven Speed Chain");
             }
-            case 12: {
+            case 12 -> {
                 i.setSrc("12_chain.png");
                 i.setAltText("Twelve Speed Chain");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
@@ -236,73 +241,76 @@ public class ImageService {
     private Image chooseCassetteImage(FullBike b) {
         Image i = new Image(7, "Cassette", "no_image.png", "No Gears Selected");
         switch ((int) b.getNumberOfRearGears()) {
-            case 1: {
+            case 1 -> {
                 i.setSrc("1_cassette.png");
                 i.setAltText("Single Speed Cog");
             }
-            case 9: {
+            case 9 -> {
                 i.setSrc("9_cassette.png");
                 i.setAltText("Nine Speed Cassette");
             }
-            case 10: {
+            case 10 -> {
                 i.setSrc("10_cassette.png");
                 i.setAltText("Ten Speed Cassette");
             }
-            case 11: {
+            case 11 -> {
                 i.setSrc("11_cassette.png");
                 i.setAltText("Eleven Speed Cassette");
             }
-            case 12: {
+            case 12 -> {
                 i.setSrc("12_cassette.png");
                 i.setAltText("Twelve Speed Cassette");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
 
     private Image chooseChainsetImage(FullBike b) {
         Image i = new Image(8, "Chain Set", "no_image.png", "No Gears Selected");
-        switch ((int) b.getNumberOfRearGears()) {
-            case 1: {
+        switch ((int) b.getNumberOfFrontGears()) {
+            case 1 -> {
                 i.setSrc("1_chainset.png");
                 i.setAltText("Single Speed Chainset");
             }
-            case 2: {
+            case 2 -> {
                 i.setSrc("2_chainset.png");
                 i.setAltText("Double Chainset");
             }
-            case 3: {
+            case 3 -> {
                 i.setSrc("3_chainset.png");
                 i.setAltText("Triple Chainset");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
 
     private Image chooseFrontDerailleurImage(FullBike b) {
         Image i = new Image(9, "Front Derailleur", "no_image.png", "No Gears Selected");
-        switch ((int) b.getNumberOfRearGears()) {
-            case 1: {
+        switch ((int) b.getNumberOfFrontGears()) {
+            case 1 -> {
                 i.setSrc("1_derailleur.png");
                 i.setAltText("Chain Catch - Front");
             }
-            case 2: {
+            case 2 -> {
                 i.setSrc("2_derailleur.png");
                 i.setAltText("Double Front Derailleur");
             }
-            case 3: {
+            case 3 -> {
                 i.setSrc("3_derailleur.png");
                 i.setAltText("Triple Front Derailleur");
             }
-            default:
+            default -> {
                 i.setSrc("no_image.png");
                 i.setAltText("No Gears Selected");
+            }
         }
         return i;
     }
