e0c3ee71cd21aec930fe98b1789de84265e74215
JML42 <joshua.mcculloch@sky.uk>
2023-12-19 16:59:26 +0000

    Removes the requirement for a DB and respository DAO

--- src/main/java/com/homeapp/nonsense_BE/repository/FullBikeDao.java
+++
@@ -1,7 +0,0 @@
-package com.homeapp.nonsense_BE.repository;
-
-import com.homeapp.nonsense_BE.models.bike.FullBike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FullBikeDao extends JpaRepository<FullBike, Long> {
-}
