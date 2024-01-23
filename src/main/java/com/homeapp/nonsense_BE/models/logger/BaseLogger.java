981707e1438c371eeb72bb2e43eecad42bf759d7
JML42 <joshua.mcculloch@sky.uk>
2024-01-23 15:42:47 +0000

    Makes all methods abstract and implements sychronized for the public log() method

--- src/main/java/com/homeapp/nonsense_BE/models/logger/BaseLogger.java
+++ src/main/java/com/homeapp/nonsense_BE/models/logger/BaseLogger.java
@@ -1,46 +1,21 @@
 package com.homeapp.nonsense_BE.models.logger;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
 
 public abstract class BaseLogger {
+    static final DateTimeFormatter LOGS_STAMP_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
     static final DateTimeFormatter FILE_NAME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    private static final DateTimeFormatter LOGS_STAMP_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-    private final Set<String> logsToWrite;
 
     public BaseLogger() {
-        this.logsToWrite = new TreeSet<>();
     }
+
+    protected abstract TreeSet<String> readLogsFile();
 
     protected abstract String getFileName();
 
-    private void logToFile() {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(getFileName(), true));
-            for (String log : logsToWrite) {
-                List<String> lines = Arrays.stream(log.split("!!")).toList();
-                for (String l : lines) {
-                    bw.write(l);
-                    bw.newLine();
-                }
-            }
-            logsToWrite.clear();
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-    }
+    protected abstract void logToFile();
 
-    public synchronized void log(String message) {
-        message = "[" + LocalDateTime.now().format(LOGS_STAMP_FORMATTER) + "] - " + message;
-        logsToWrite.add(message);
-        logToFile();
-    }
 
+    protected abstract void log(String message);
 }
--- src/main/java/com/homeapp/nonsense_BE/models/logger/InfoLogger.java
+++ src/main/java/com/homeapp/nonsense_BE/models/logger/InfoLogger.java
@@ -1,11 +1,60 @@
 package com.homeapp.nonsense_BE.models.logger;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TreeSet;
 
 public class InfoLogger extends BaseLogger {
+    private final ObjectMapper om = new ObjectMapper();
+    private final TreeSet<String> logs;
+
+    public InfoLogger() {
+        this.logs = readLogsFile();
+    }
+
+    @Override
+    protected TreeSet<String> readLogsFile() {
+        try {
+            File file = new File(getFileName());
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            if (file.length() == 0) {
+                return new TreeSet<>();
+            }
+            return om.readValue(file, new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     protected String getFileName() {
         return "src/main/logs/" + LocalDate.now().format(FILE_NAME_FORMATTER) + "_INFO.json";
+    }
+
+    @Override
+    protected void logToFile() {
+        try {
+            om.writeValue(new File(getFileName()), logs);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    @Override
+    public void log(String message) {
+        synchronized (this) {
+            message = "[" + LocalDateTime.now().format(LOGS_STAMP_FORMATTER) + "] - " + message;
+            logs.add(message);
+            logToFile();
+        }
     }
 }
